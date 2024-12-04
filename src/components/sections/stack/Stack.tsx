8116853619fa83b780e00137cd54b9b103201b78
David Leder <davidleder98@gmail.com>
2024-12-04 21:58:39 +0100

    stack

--- src/components/sections/stack/Stack.tsx
+++ src/components/sections/stack/Stack.tsx
@@ -10,7 +10,7 @@ const Stack = () => {
         <section id="tech-stack">
             <div className="stack-container">
                 <h1>Tech Stack</h1> 
-                <div className="stack-list">
+                <div className="stack-list-large">
                     <div className="stack-item">
                         <img src={net} />
                         <p>.NET</p>
@@ -30,6 +30,32 @@ const Stack = () => {
                     <div className="stack-item">
                         <img src={atom} />
                         <p>React</p>
+                    </div>
+                </div>
+                <div className="stack-list-small">
+                    <div className="sl-top">
+                        <div className="stack-item">
+                            <img src={net} />
+                            <p>.NET</p>
+                        </div>
+                        <div className="stack-item">
+                            <img src={azure} />
+                            <p>Azure</p>
+                        </div>
+                    </div>
+                    <div className="sl-bot">
+                        <div className="stack-item">
+                            <img src={html} />
+                            <p>HTML</p>
+                        </div>
+                        <div className="stack-item">
+                            <img src={css} />
+                            <p>CSS</p>
+                        </div>
+                        <div className="stack-item">
+                            <img src={atom} />
+                            <p>React</p>
+                        </div>
                     </div>
                 </div>
             </div>
